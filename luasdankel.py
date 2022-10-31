012f93f308e7c105834ada4c946e566c9e616f29
luffy <rajadesign80@gmail.com>
2022-10-31 11:33:52 +0700

    new

---
+++ luasdankel.py
@@ -0,0 +1,11 @@
+##tugas raja heppyanto 
+##TI.22.A2
+## program hitung luas lingkaran dan keliling lingkaran
+
+r=float(input("masukkan jari jari lingkaran:"))
+
+phi=22/7
+luas=phi*r*r
+keliling=2*phi*r
+print("luas lingkaran=","{:.3f}".format(luas))
+print("keliling lingkaran=","{:.3f}".format(keliling))
